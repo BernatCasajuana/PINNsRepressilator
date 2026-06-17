a88dba1ea24b1cf7ba441fd73892488c63b36bf5
BernatCasajuana <casajuana.bernat@gmail.com>
2026-06-17 23:45:23 +0200

    job name

--- jobs/exp2_partial_observation_job.sh
+++ jobs/exp2_partial_observation_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=exp_partial_obs            # Nom del job
-#SBATCH --output=/home/10040984@uvic.local/projects/pinns-repressilator/jobs/exp_partial_obs_output.txt
-#SBATCH --error=/home/10040984@uvic.local/projects/pinns-repressilator/jobs/exp_partial_obs_error.txt
+#SBATCH --job-name=exp2_partial_obs            # Nom del job
+#SBATCH --output=/home/10040984@uvic.local/projects/pinns-repressilator/jobs/exp2_partial_obs_output.txt
+#SBATCH --error=/home/10040984@uvic.local/projects/pinns-repressilator/jobs/exp2_partial_obs_error.txt
 #SBATCH --time=12:00:00                       # Temps maxim (hh:mm:ss)
 #SBATCH --cpus-per-task=1                     # Nombre de CPUs per tasca
 #SBATCH --mem=8GB                             # Memoria assignada

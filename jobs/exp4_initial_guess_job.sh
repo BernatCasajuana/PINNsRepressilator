ff55474886d47c3d42d83eb172f345938ca44651
BernatCasajuana <casajuana.bernat@gmail.com>
2026-06-17 12:21:51 +0200

    exp 4 job time

--- jobs/exp4_initial_guess_job.sh
+++ jobs/exp4_initial_guess_job.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=exp4_initial_guess          # Nom del job
 #SBATCH --output=/home/10040984@uvic.local/projects/pinns-repressilator/jobs/exp4_initial_guess_output.txt
 #SBATCH --error=/home/10040984@uvic.local/projects/pinns-repressilator/jobs/exp4_initial_guess_error.txt
-#SBATCH --time=12:00:00                       # Temps maxim (hh:mm:ss)
+#SBATCH --time=24:00:00                       # Temps maxim (hh:mm:ss)
 #SBATCH --cpus-per-task=1                     # Nombre de CPUs per tasca
 #SBATCH --mem=8GB                             # Memoria assignada
 
